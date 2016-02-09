942c4d168ac30af8db50fb2da6d29d5566601dff
Ignacio Carbajo <ignacio.carbajo@momentumft.co.uk>
2016-02-09 13:28:25 +0000

    Add comments and react extra

--- index.js
+++ index.js
@@ -20,78 +20,80 @@ module.exports = {
     "strict": 0,
 
     // Possible Errors
-    "comma-dangle": [2, "always-multiline"],
-    "no-cond-assign": 2,
-    "no-constant-condition": 2,
-    "no-dupe-args": 2,
-    "no-dupe-keys": 2,
-    "no-duplicate-case": 2,
-    "no-empty": 2,
-    "no-ex-assign": 2,
-    "no-extra-boolean-cast": 2,
-    "no-extra-parens": [2, "functions"],
-    "no-func-assign": 2,
-    "no-inner-declarations": [2, "functions"],
-    "no-obj-calls": 2,
-    "no-undef": 2,
-    "no-unexpected-multiline": 2,
-    "no-unreachable": 2,
-    "no-use-before-define": [2, "nofunc"],
-    "no-unused-vars": 2,
-    "use-isnan": 2,
-    "valid-typeof": 2,
+    "comma-dangle": [2, "always-multiline"], // disallow or enforce trailing commas
+    "no-cond-assign": 2, // disallow assignment in conditional expressions
+    "no-constant-condition": 2, // disallow use of constant expressions in conditions
+    "no-dupe-args": 2, // disallow duplicate arguments in functions
+    "no-dupe-keys": 2, // disallow duplicate keys when creating object literals
+    "no-duplicate-case": 2, // disallow a duplicate case label
+    "no-empty": 2, // disallow empty statements
+    "no-ex-assign": 2, // disallow assigning to the exception in a catch block
+    "no-extra-boolean-cast": 2, // disallow double-negation boolean casts in a boolean context
+    "no-extra-parens": [2, "functions"], // disallow unnecessary parentheses
+    "no-func-assign": 2, // disallow overwriting functions written as function declarations
+    "no-inner-declarations": [2, "functions"], // disallow function or variable declarations in nested blocks
+    "no-obj-calls": 2, // disallow the use of object properties of the global object (Math and JSON) as functions 
+    "no-undef": 2, // disallow use of undeclared variables unless mentioned in a /*global */ block
+    "no-unexpected-multiline": 2, // avoid code that looks like two expressions but is actually one
+    "no-unreachable": 2, // disallow unreachable statements after a return, throw, continue, or break statement
+    "no-unused-vars": 2, // disallow declaration of variables that are not used in the code
+    "no-use-before-define": [2, "nofunc"], // disallow use of variables before they are defined
+    "use-isnan": 2, // disallow comparisons with the value NaN
+    "valid-typeof": 2, // Ensure that the results of typeof are compared against a valid string
 
     // Best Practices
-    "accessor-pairs": 2,
-    "consistent-return": 2,
-    "dot-notation": 2,
+    "accessor-pairs": 2, // Enforces getter/setter pairs in objects
+    "consistent-return": 2, // require return statements to either always or never specify values
     "complexity": [2,
       7
-    ],
-    "eqeqeq": [2, "smart"],
-    "no-else-return": 2,
-    "no-extra-bind": 2,
-    "no-lone-blocks": 2,
+    ], // specify the maximum cyclomatic complexity allowed in a program
+    "dot-notation": 2, // encourages use of dot notation whenever possible
+    "eqeqeq": [2, "smart"], // require the use of === and !==
+    "no-else-return": 2, // disallow else after a return in an if
+    "no-extra-bind": 2, // disallow unnecessary function binding
+    "no-lone-blocks": 2, // disallow unnecessary nested blocks
 
     // Stylistic Issues
-    "array-bracket-spacing": [2, "never"],
-    "brace-style": [2, "1tbs"],
-    "camelcase": 2,
-    "comma-spacing": 2,
-    "comma-style": 2,
-    "eol-last": 2,
+    "array-bracket-spacing": [2, "never"], // enforce spacing inside array brackets
+    "brace-style": [2, "1tbs"], // enforce one true brace style
+    "camelcase": 2, // require camel case names
+    "comma-spacing": 2, // enforce spacing before and after comma
+    "comma-style": 2, // enforce one true comma style
+    "eol-last": 2, // enforce newline at the end of file, with no multiple empty lines
     "func-style": [2, "declaration",  {
         "allowArrowFunctions": true
       }
-    ],
-    "indent": [2, 2],
-    "jsx-quotes": [2, "prefer-double"],
-    "key-spacing": 2,
-    "linebreak-style": [2, "unix"],
-    "lines-around-comment": 2,
-    "max-nested-callbacks": [2, 3],
-    "new-cap": [2, { "capIsNew": false }],
-    "new-parens": 2,
-    "no-multiple-empty-lines": [1, { "max": 3 }],
-    "no-nested-ternary": 2,
-    "no-unneeded-ternary": 2,
-    "no-underscore-dangle": 0,
-    "quotes": [2, "double"],
-    "semi": [2, "never"],
-    "sort-vars": 2,
-    "space-after-keywords": 2,
-    "space-before-blocks": 2,
-    "space-before-function-paren": [2, "never"],
-    "space-in-parens": [2, "never"],
-    "space-infix-ops": 2,
-    "spaced-comment": 2,
+    ], // enforce use of function declarations or expressions
+    "indent": [2, 2], // specify tab or space width for your code
+    "jsx-quotes": [2, "prefer-double"], // specify whether double or single quotes should be used in JSX attributes
+    "key-spacing": 2, // enforce spacing between keys and values in object literal properties
+    "linebreak-style": [2, "unix"], // disallow mixed 'LF' and 'CRLF' as linebreaks
+    "lines-around-comment": 2, // enforce empty lines around comments
+    "max-nested-callbacks": [2, 3], // specify the maximum depth callbacks can be nested
+    "new-cap": [2, { "capIsNew": false }], // require a capital letter for constructors
+    "new-parens": 2, // disallow the omission of parentheses when invoking a constructor with no arguments
+    "no-multiple-empty-lines": [1, { "max": 3 }], // disallow multiple empty lines
+    "no-nested-ternary": 2, // disallow nested ternary expressions
+    "no-underscore-dangle": 0, // disallow dangling underscores in identifiers
+    "no-unneeded-ternary": 2, // disallow the use of ternary operators when a simpler alternative exists
+    "quotes": [2, "double"], // specify whether backticks, double or single quotes should be used 
+    "semi": [2, "never"], // require or disallow use of semicolons instead of ASI
+    "sort-vars": 2, // sort variables within the same declaration block
+    "space-after-keywords": 2, // require a space after certain keywords
+    "space-before-blocks": 2, // space-before-blocks
+    "space-before-function-paren": [2, "never"], // require or disallow a space before function opening parenthesis
+    "space-in-parens": [2, "never"], // require or disallow spaces inside parentheses
+    "space-infix-ops": 2, // require spaces around operators
+    "spaced-comment": 2, // require or disallow a space immediately following the // or /* in a comment
 
     // ECMAScript 6
-    "constructor-super": 2,
-    "no-this-before-super": 2,
-    "no-var": 2,
-    "babel/object-shorthand": 2,
-    "prefer-const": 2
+    "constructor-super": 2, // verify calls of super() in constructors
+    "no-this-before-super": 2, // disallow use of this/super before calling super() in constructors.
+    "no-var": 2, // require let or const instead of var
+    "prefer-const": 2, // uggest using const declaration for variables that are never modified after declared
 
+    // Plugins
+    // Babel
+    "babel/object-shorthand": 2, // enforce concise object methods and properties
   }
 }
--- react.js
+++ react.js
@@ -9,17 +9,48 @@ module.exports = {
     "jsx": true
   },
 
+  // Plugins
+  // React
   "rules": {
-    "react/jsx-curly-spacing": [2, "never"],
-    "react/jsx-no-undef": 2,
-    "react/jsx-uses-react": 2,
-    "react/no-did-mount-set-state": 2,
-    "react/no-did-update-set-state": 2,
-    "react/no-multi-comp": 2,
-    "react/no-unknown-property": 2,
-    "react/prop-types": 2,
-    "react/react-in-jsx-scope": 2,
-    "react/self-closing-comp": 2,
-    "react/wrap-multilines": 2
+    "react/display-name": 0, // Prevent missing displayName in a React component definition
+    "react/forbid-prop-types": 0, // Forbid certain propTypes
+    "react/jsx-boolean-value": 0, // Enforce boolean attributes notation in JSX (fixable)
+    "react/jsx-closing-bracket-location": 0, // Validate closing bracket location in JSX
+    "react/jsx-curly-spacing": [2, "never"], // Enforce or disallow spaces inside of curly braces in JSX attributes (fixable)
+    "react/jsx-equals-spacing": 0, // Enforce or disallow spaces around equal signs in JSX attributes
+    "react/jsx-handler-names": 0, // Enforce event handler naming conventions in JSX
+    "react/jsx-indent": 0, // Validate JSX indentation
+    "react/jsx-indent-props": 0, // Validate props indentation in JSX
+    "react/jsx-key": 0, // Validate JSX has key prop when in array or iterator
+    "react/jsx-max-props-per-line": 0, // Limit maximum of props on a single line in JSX
+    "react/jsx-no-bind": 0, // Prevent usage of .bind() and arrow functions in JSX props
+    "react/jsx-no-duplicate-props": 0, // Prevent duplicate props in JSX
+    "react/jsx-no-literals": 0, // Prevent usage of unwrapped JSX strings
+    "react/jsx-no-undef": 0, // Disallow undeclared variables in JSX
+    "react/jsx-no-undef": 2, // Disallow undeclared variables in JSX
+    "react/jsx-pascal-case": 0, // Enforce PascalCase for user-defined JSX components
+    "react/jsx-quotes": 0, // Enforce quote style for JSX attributes
+    "react/jsx-sort-prop-types": 0, // Enforce propTypes declarations alphabetical sorting
+    "react/jsx-sort-props": 0, // Enforce props alphabetical sorting
+    "react/jsx-space-before-closing": 0, // Validate spacing before closing bracket in JSX (fixable)
+    "react/jsx-uses-react": 2, // Prevent React to be incorrectly marked as unused
+    "react/jsx-uses-vars": 0, // Prevent variables used in JSX to be incorrectly marked as unused
+    "react/no-danger": 0, // Prevent usage of dangerous JSX properties
+    "react/no-deprecated": 0, // Prevent usage of deprecated methods
+    "react/no-did-mount-set-state": 2, // Prevent usage of setState in componentDidMount
+    "react/no-did-update-set-state": 2, // Prevent usage of setState in componentDidUpdate
+    "react/no-direct-mutation-state": 0, // Prevent direct mutation of this.state
+    "react/no-is-mounted": 0, // Prevent usage of isMounted
+    "react/no-multi-comp": 2, // Prevent multiple component definition per file
+    "react/no-set-state": 0, // Prevent usage of setState
+    "react/no-string-refs": 0, // Prevent using string references in ref attribute.
+    "react/no-unknown-property": 2, // Prevent usage of unknown DOM property (fixable)
+    "react/prefer-es6-class": 0, // Enforce ES5 or ES6 class for React Components
+    "react/prop-types": 2, // Prevent missing props validation in a React component definition
+    "react/react-in-jsx-scope": 2, // Prevent missing React when using JSX
+    "react/require-extension": 0, // Restrict file extensions that may be required
+    "react/self-closing-comp": 2, // Prevent extra closing tags for components without children
+    "react/sort-comp": 0, // Enforce component methods order
+    "react/wrap-multilines": 2, // Prevent missing parentheses around multilines JSX (fixable)
   }
 }
