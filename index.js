3964e94bb2646817a4c87693580e7200d95c4613
Thom Wright <thom.wright@momentumft.co.uk>
2016-03-31 14:04:35 +0100

    Warn about invalid JSDoc
    Be relaxed about returns (often just noise)
    Prefer consistent casing for JS types

--- index.js
+++ index.js
@@ -22,34 +22,44 @@ module.exports = {
     // Possible Errors
     "comma-dangle": [2, "always-multiline"], // disallow or enforce trailing commas
     "no-cond-assign": 2, // disallow assignment in conditional expressions
-    "no-console": 0, // disallow use of console in the node environment 
+    "no-console": 0, // disallow use of console in the node environment
     "no-constant-condition": 2, // disallow use of constant expressions in conditions
-    "no-control-regex": 0, // disallow control characters in regular expressions 
-    "no-debugger": 0, // disallow use of debugger 
+    "no-control-regex": 0, // disallow control characters in regular expressions
+    "no-debugger": 0, // disallow use of debugger
     "no-dupe-args": 2, // disallow duplicate arguments in functions
     "no-dupe-keys": 2, // disallow duplicate keys when creating object literals
     "no-duplicate-case": 2, // disallow a duplicate case label
     "no-empty": 2, // disallow empty statements
-    "no-empty-character-class": 0, // disallow the use of empty character classes in regular expressions 
+    "no-empty-character-class": 0, // disallow the use of empty character classes in regular expressions
     "no-ex-assign": 2, // disallow assigning to the exception in a catch block
     "no-extra-boolean-cast": 2, // disallow double-negation boolean casts in a boolean context
     "no-extra-parens": [2, "functions"], // disallow unnecessary parentheses
-    "no-extra-semi": 0, // disallow unnecessary semicolons  
+    "no-extra-semi": 0, // disallow unnecessary semicolons
     "no-func-assign": 2, // disallow overwriting functions written as function declarations
     "no-inner-declarations": [2, "functions"], // disallow function or variable declarations in nested blocks
-    "no-invalid-regexp": 0, // disallow invalid regular expression strings in the RegExp constructor 
-    "no-irregular-whitespace": 0, // disallow irregular whitespace outside of strings and comments 
-    "no-negated-in-lhs": 0, // disallow negation of the left operand of an in expression 
-    "no-obj-calls": 2, // disallow the use of object properties of the global object (Math and JSON) as functions 
-    "no-regex-spaces": 0, // disallow multiple spaces in a regular expression literal 
-    "no-sparse-arrays": 0, // disallow sparse arrays 
+    "no-invalid-regexp": 0, // disallow invalid regular expression strings in the RegExp constructor
+    "no-irregular-whitespace": 0, // disallow irregular whitespace outside of strings and comments
+    "no-negated-in-lhs": 0, // disallow negation of the left operand of an in expression
+    "no-obj-calls": 2, // disallow the use of object properties of the global object (Math and JSON) as functions
+    "no-regex-spaces": 0, // disallow multiple spaces in a regular expression literal
+    "no-sparse-arrays": 0, // disallow sparse arrays
     "no-undef": 2, // disallow use of undeclared variables unless mentioned in a /*global */ block
     "no-unexpected-multiline": 2, // avoid code that looks like two expressions but is actually one
     "no-unreachable": 2, // disallow unreachable statements after a return, throw, continue, or break statement
     "no-unused-vars": 2, // disallow declaration of variables that are not used in the code
     "no-use-before-define": [2, "nofunc"], // disallow use of variables before they are defined
     "use-isnan": 2, // disallow comparisons with the value NaN
-    "valid-jsdoc": 0, // Ensure JSDoc comments are valid
+    "valid-jsdoc": ["warn", {
+      "requireReturn": false,
+      "requireReturnDescription": false,
+      "requireReturnType": false,
+      "preferType": {
+        "String": "String",
+        "Object": "Object",
+        "Array": "Array",
+        "Number": "Number"
+      }
+    }], // Ensure JSDoc comments are valid
     "valid-typeof": 2, // Ensure that the results of typeof are compared against a valid string
 
     // Best Practices
@@ -74,7 +84,7 @@ module.exports = {
     "no-eval": 0, // disallow use of eval()
     "no-extend-native": 0, // disallow adding to native types
     "no-extra-bind": 2, // disallow unnecessary function binding
-    "no-fallthrough": 0, // disallow fallthrough of case statements 
+    "no-fallthrough": 0, // disallow fallthrough of case statements
     "no-floating-decimal": 0, // disallow the use of leading or trailing decimal points in numeric literals
     "no-implicit-coercion": 0, // disallow the type conversions with shorter notations
     "no-implied-eval": 0, // disallow use of eval()-like methods
@@ -84,18 +94,18 @@ module.exports = {
     "no-lone-blocks": 2, // disallow unnecessary nested blocks
     "no-loop-func": 0, // disallow creation of functions within loops
     "no-magic-numbers ": 0, // isallow the use of magic numbers
-    "no-multi-spaces": 0, // disallow use of multiple spaces 
+    "no-multi-spaces": 0, // disallow use of multiple spaces
     "no-multi-str": 0, // disallow use of multiline strings
     "no-native-reassign": 0, // disallow reassignments of native objects
     "no-new": 0, // disallow use of the new operator when not part of an assignment or comparison
     "no-new-func": 0, // disallow use of new operator for Function object
     "no-new-wrappers": 0, // disallows creating new instances of String,Number, and Boolean
-    "no-octal": 0, // disallow use of octal literals 
+    "no-octal": 0, // disallow use of octal literals
     "no-octal-escape": 0, // disallow use of octal escape sequences in string literals, such as var foo = "Copyright \251";
     "no-param-reassign": 0, // disallow reassignment of function parameters
     "no-process-env": 0, // disallow use of process.env
     "no-proto": 0, // disallow usage of __proto__ property
-    "no-redeclare": 0, // disallow declaring the same variable more than once 
+    "no-redeclare": 0, // disallow declaring the same variable more than once
     "no-return-assign": 0, // disallow use of assignment in return statement
     "no-script-url": 0, // disallow use of javascript: urls.
     "no-self-compare": 0, // disallow comparisons where both sides are exactly the same
@@ -114,12 +124,12 @@ module.exports = {
 
     // Stylistic Issues
     "array-bracket-spacing": [2, "never"], // enforce spacing inside array brackets
-    "block-spacing": 0, // disallow or enforce spaces inside of single line blocks 
+    "block-spacing": 0, // disallow or enforce spaces inside of single line blocks
     "brace-style": [2, "1tbs"], // enforce one true brace style
     "camelcase": 2, // require camel case names
     "comma-spacing": 2, // enforce spacing before and after comma
     "comma-style": 2, // enforce one true comma style
-    "computed-property-spacing": 0, // require or disallow padding inside computed properties 
+    "computed-property-spacing": 0, // require or disallow padding inside computed properties
     "consistent-this": 0, // enforce consistent naming when capturing the current execution context
     "eol-last": 2, // enforce newline at the end of file, with no multiple empty lines
     "func-names": 0, // require function expressions to have a name
@@ -144,26 +154,26 @@ module.exports = {
     "no-continue": 0, // disallow use of the continue statement
     "no-inline-comments": 0, // disallow comments inline after code
     "no-lonely-if": 0, // disallow if as the only statement in an else block
-    "no-mixed-spaces-and-tabs": 0, // disallow mixed spaces and tabs for indentation 
+    "no-mixed-spaces-and-tabs": 0, // disallow mixed spaces and tabs for indentation
     "no-multiple-empty-lines": [1, { "max": 3 }], // disallow multiple empty lines
     "no-negated-condition": 0, // disallow negated conditions
     "no-nested-ternary": 2, // disallow nested ternary expressions
     "no-new-object": 0, // disallow the use of the Object constructor
     "no-plusplus": 0, // disallow use of unary operators, ++ and --
     "no-restricted-syntax": 0, // disallow use of certain syntax in code
-    "no-spaced-func": 0, // disallow space between function identifier and application 
+    "no-spaced-func": 0, // disallow space between function identifier and application
     "no-ternary": 0, // disallow the use of ternary operators
-    "no-trailing-spaces": 0, // disallow trailing whitespace at the end of lines 
+    "no-trailing-spaces": 0, // disallow trailing whitespace at the end of lines
     "no-underscore-dangle": 0, // disallow dangling underscores in identifiers
     "no-underscore-dangle": 0, // disallow dangling underscores in identifiers
     "no-unneeded-ternary": 2, // disallow the use of ternary operators when a simpler alternative exists
-    "object-curly-spacing": 0, // require or disallow padding inside curly braces 
+    "object-curly-spacing": 0, // require or disallow padding inside curly braces
     "one-var": 0, // require or disallow one variable declaration per function
     "operator-assignment": 0, // require assignment operator shorthand where possible or prohibit it entirely
     "operator-linebreak": 0, // enforce operators to be placed before or after line breaks
     "padded-blocks": 0, // enforce padding within blocks
     "quote-props": 0, // require quotes around object literal property names
-    "quotes": [2, "double"], // specify whether backticks, double or single quotes should be used 
+    "quotes": [2, "double"], // specify whether backticks, double or single quotes should be used
     "require-jsdoc": 0, // Require JSDoc comment
     "semi": [2, "never"], // require or disallow use of semicolons instead of ASI
     "semi-spacing": 0, // enforce spacing before and after semicolons
@@ -171,20 +181,20 @@ module.exports = {
     "space-after-keywords": 2, // require a space after certain keywords
     "space-before-blocks": 2, // space-before-blocks
     "space-before-function-paren": [2, "never"], // require or disallow a space before function opening parenthesis
-    "space-before-keywords": 0, // require a space before certain keywords 
+    "space-before-keywords": 0, // require a space before certain keywords
     "space-in-parens": [2, "never"], // require or disallow spaces inside parentheses
     "space-infix-ops": 2, // require spaces around operators
-    "space-return-throw-case": 0, // require a space after return, throw, and case 
-    "space-unary-ops": 0, // require or disallow spaces before/after unary operators 
+    "space-return-throw-case": 0, // require a space after return, throw, and case
+    "space-unary-ops": 0, // require or disallow spaces before/after unary operators
     "spaced-comment": 2, // require or disallow a space immediately following the // or /* in a comment
     "wrap-regex": 0, // require regex literals to be wrapped in parentheses
 
     // ECMAScript 6
     "arrow-body-style": 0, // require braces in arrow function body
     "arrow-parens": 0, // require parens in arrow function arguments
-    "arrow-spacing": 0, // require space before/after arrow function's arrow 
+    "arrow-spacing": 0, // require space before/after arrow function's arrow
     "constructor-super": 2, // verify calls of super() in constructors
-    "generator-star-spacing": 0, // enforce spacing around the * in generator functions 
+    "generator-star-spacing": 0, // enforce spacing around the * in generator functions
     "no-arrow-condition": 0, // disallow arrow functions where a condition is expected
     "no-class-assign": 0, // disallow modifying variables of class declarations
     "no-const-assign": 0, // disallow modifying variables that are declared using const
