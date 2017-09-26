63fc607d48bf8e5439e6addc1cf3fc45adb444d4
Dave Tonge <dave.tonge@momentumft.co.uk>
2017-09-26 09:56:49 +0100

    Add a warning for sorting keys

--- fp.js
+++ fp.js
@@ -27,5 +27,6 @@ module.exports = {
     "no-magic-numbers": [2, {  "ignore": [0,1] }],
     "no-unused-expressions": 2, // disallow usage of expressions in statement position   
     "no-lonely-if": 2, // disallow if as the only statement in an else block    
+    "sort-keys": [1, "asc", {"caseSensitive": true, "natural": false}],    
   },
 }
